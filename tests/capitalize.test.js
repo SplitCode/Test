413d5decc74eaf18175ed059ea115f3315872aca
SplitCode <natacsp@mail.ru>
2023-08-24 10:22:53 +0300

    fix: replaced if to assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,16 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает криво!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает криво!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает криво!');
+// }
+
+assert.equal(capitalize(''), '');
+
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает криво!');
+// }
 
 console.log('Все тесты пройдены!');
